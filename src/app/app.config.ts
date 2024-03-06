643ac4eb3e00dacc1e146f5b036926250ed80a31
MicaelaSalvvador <sacmi9@hotmail.com>
2024-03-06 10:10:42 -0600

    interceptor master angular 17

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
+import { errorHandlerInterceptor } from './core/interceptors/error-handler.interceptor';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),provideHttpClient(withInterceptors([errorHandlerInterceptor]))]
 };
